f27ae1bfa2cf45a194a203520ca2ff233ac6abca
supratim531 <91423964+supratim531@users.noreply.github.com>
2024-10-23 21:42:34 +0530

    supratim531: fix(main): Removed 1 log from the post end-point

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ async def form_submit(request: Request):
 	data = await request.json()  # Get the data sent by the Google Form
 	print("1st", data)
 	print("2nd", dir(data))
-	print("3rd", data.__dict__)
 
 	# Trigger your Python script or perform some action
 	subprocess.run(["python3", "script.py"])
