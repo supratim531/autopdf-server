776c5e0fd5a6ba809ab2f886de06c133fc6228e4
supratim531 <91423964+supratim531@users.noreply.github.com>
2024-10-23 12:28:43 +0530

    supratim531: Initial commit from local repo

---
+++ main.py
@@ -0,0 +1,30 @@
+import subprocess
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = ['*']
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+@app.get("/health")
+async def read_root():
+	return {"message": "Welcome to FastAPI server"}
+
+@app.get("/health")
+async def health():
+	return {"message": "FastAPI server is up & running"}
+
+@app.post("/form-submit")
+async def form_submit(request: Request):
+	data = await request.json()  # Get the data sent by the Google Form
+	# Trigger your Python script or perform some action
+	subprocess.run(["python3", "script.py"])
+	return {"message": "Success"}
