2a69b1340ea57315385d7558162b4c0ed4054bac
supratim531 <91423964+supratim531@users.noreply.github.com>
2024-10-23 21:30:14 +0530

    supratim531: fix(main): Added logs to the post end-point

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ async def health():
 @app.post("/form-submit")
 async def form_submit(request: Request):
 	data = await request.json()  # Get the data sent by the Google Form
+	print("1st", data)
+	print("2nd", dir(data))
+	print("3rd", data.__dict__)
+
 	# Trigger your Python script or perform some action
 	subprocess.run(["python3", "script.py"])
 	return {"message": "Success"}
